e3eaa19f59219033be03a955d088e0f0d1302b37
Samuel Leavin <sam.leavin@att.net>
2015-05-08 15:04:35 -0400

    updated ourambient.js

--- ambient/ourAmbient.js
+++ ambient/ourAmbient.js
@@ -10,7 +10,7 @@ specified light or sound level trigger is met.
 var tessel = require('tessel');
 var ambientlib = require('ambient-attx4');
 
-var ambient = ambientlib.use(tessel.port['A']);
+var ambient = ambientlib.use(tessel.port['C']);
 
 ambient.on('ready', function () {
 	console.log('light sensor is ready!');
@@ -20,14 +20,19 @@ ambient.on('ready', function () {
 
 		ambient.getLightLevel(function(err, ldata){
 			if (err) throw err;
+			console.log(ldata.toFixed(4));
 			lightData.push(ldata.toFixed(4));
 		})
 
-	}, 250)
+	}, 250);
 
-	setTimeout(clearInterval(getLight), 1500)
-	console.log('our light data:')
-	console.log(lightData)
+	getLight();
+
+	setTimeout(clearInterval(getLight), 1500);
+	setTimeout(function(){
+		console.log('our light data:');
+		console.log(lightData);
+	}, 2000)
 });
 
 ambient.on('error', function (err) {
